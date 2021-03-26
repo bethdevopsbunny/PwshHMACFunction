cf43efe76efa340c003b9e60cbf6d7f16cf15c16
bethdevopsbunny <80027170+bethdevopsbunny@users.noreply.github.com>
2021-03-26 10:39:58 +0000

    Add files via upload

---
+++ PwshHMACFunction.ps1
@@ -0,0 +1,67 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+
+## Raw Empty Check
+if($null -eq $Request.rawbody){
+    
+    # return bad request if empty body
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::BadRequest
+        Body       = "Unable to validate authentication header for messages with empty body"
+    })
+    
+    
+}else{
+    
+    
+    
+    function Generate-HMAC {
+        param (
+            $RawBodyPayload,
+            $Base64Key
+            )
+            
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Convert]::FromBase64String($Base64Key)
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::UTF8.GetBytes($RawBodyPayload))
+            $HMACValue = "HMAC " + [Convert]::ToBase64String($signature)
+            
+            return $HMACValue
+            
+        }
+        
+        #$GeneratedHMAC  = Generate-HMAC -RawBodyPayload $Request.rawbody -Base64Key $ENV:SigningKey
+       
+        if ($GeneratedHMAC -eq $Request.headers.authorization){   
+            
+            
+            ## Payload data = $Request.body
+            
+            
+            ###
+            ##  passed authentication code. 
+            ### 
+
+
+
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body = $ReturnPayload
+        })
+
+    }
+    else
+    {
+        # return failed authentication
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::Unauthorized
+            Body       = "Failed Authentication"
+        })
+
+    }
+
+}
